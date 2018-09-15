b66e4f31a35bd27823850e0bf7e5eb3e45e0143e
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-09-15 15:06:42 +0500

    matrics

--- cluster.py
+++ cluster.py
@@ -4,10 +4,12 @@
 
 from download import download_data
 from xlsx2dict import convert_data
+from matrix import form_matrix
 
 URL = "http://www.gks.ru/wps/wcm/connect/rosstat_main/rosstat/ru/statistics/accounts/"
 
 if __name__ == '__main__':
 	xlsx_file = download_data(URL)
 	dict_data = convert_data(xlsx_file)
-	print(dict_data)
+	nmpy_data = form_matrix(dict_data)
+	print(nmpy_data)
