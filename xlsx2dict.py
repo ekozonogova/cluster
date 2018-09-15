6e5c785d2d0ef729b629d676efecdf1a83314484
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-09-15 21:38:08 +0500

    filter

--- xlsx2dict.py
+++ xlsx2dict.py
@@ -8,8 +8,7 @@ def OpenXlsx(_in = './DATA.xlsx'):
 	return sheet
 
 # Формируем многомерный словарь
-
-def create_dict(_in = './DATA.xlsx'):
+def create_dict(_in, filter_list):
 	sheet = OpenXlsx(_in = './DATA.xlsx')
 	row_number = sheet.nrows
 	col_number = sheet.ncols
@@ -22,9 +21,10 @@ def create_dict(_in = './DATA.xlsx'):
 		for i in numbers:
 			row_data = {}
 			for j in range(1, col_number):
-				print(i,j)
-				row_data.update({int(sheet.cell_value(k, 5)):sheet.cell_value(k, j)})
-			dictOut.update({i: row_data})
+				if j not in filter_list:
+					row_data.update({int(sheet.cell_value(k, 5)):sheet.cell_value(k, j)})
+			if i not in filter_list:
+				dictOut.update({i: row_data})
 			k += 1
 		return dictOut
 	else:
@@ -32,28 +32,15 @@ def create_dict(_in = './DATA.xlsx'):
 
 # Формируем список на удаление
 
-def filter_dict():
+def create_filter_list():
 	a = [49, 84, 92]
 	for i in range (106, 141):
 		a.append(i)
 	return a
 
-# Удаление элементов
-
-def update_dict(delNumbers, dictOut):
-	for i in delNumbers:
-		try:
-			del dictOut[i]
-		except:
-			break
-	return dictOut
-
-# Запуск
-
 def convert_data(_in = './DATA.xlsx'):
-	newDict = create_dict(_in = './DATA.xlsx')
-	delNumbers = filter_dict()
-	newDict = update_dict(delNumbers, newDict)
+	filter_list = create_filter_list()
+	newDict = create_dict('./DATA.xlsx', filter_list)
 	return newDict
 
 if __name__ == '__main__':
