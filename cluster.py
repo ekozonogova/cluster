52a78861e1620f226b0b77e5276781eccd53c540
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-09-18 11:23:53 +0500

    save data

--- cluster.py
+++ cluster.py
@@ -1,7 +1,7 @@
 # ИДЕНТИФИКАЦИЯ КЛАСТЕРОВ НА МАКРО-УРОВНЕ
 # Елена Козоногова elenaa.semenovaa@gmail.com
 # Даниил Курушин kurushin.daniel@yandex.ru
-from json import dump
+from utilites import dump, load
 
 from stage1 import download_data, convert_data, form_matrix
 from stage2 import *
@@ -10,9 +10,27 @@ from stage3 import *
 URL = "http://www.gks.ru/wps/wcm/connect/rosstat_main/rosstat/ru/statistics/accounts/"
 
 if __name__ == '__main__':
-	xlsx_file = download_data(URL)
-	dict_data, codes = convert_data(xlsx_file)
-	nmpy_data = form_matrix(dict_data)
+	end = False
+	while not end:
+		try:
+			xlsx_file = load('xlsx_file.json')
+			dict_data, codes = load('xlsx_content.json')
+			nmpy_data = load('nmpy_data.json')
+			end = True
+		except FileNotFoundError as e:
+			if e.filename == 'xlsx_file.json':
+				xlsx_file = download_data(URL)
+				dump(xlsx_file, 'xlsx_file.json')
+			elif e.filename == 'xlsx_content.json':
+				dict_data, codes = convert_data(xlsx_file)
+				dump((dict_data, codes), 'xlsx_content.json')
+			elif e.filename == 'nmpy_data.json':
+				nmpy_data = form_matrix(dict_data)
+				dump(nmpy_data, 'nmpy_data.json')
+			
+	exit(0)
+	
+	
 	X, Y = get_XY(nmpy_data)
 	XX = get_r(X)
 	YY = get_r(Y)
