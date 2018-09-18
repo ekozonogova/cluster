8bf9416f719f29f24ca375da254e2be0bb48e33b
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-09-18 11:05:22 +0500

    load & dump json wrappers for short call

--- unilites.py
+++ unilites.py
@@ -2,7 +2,12 @@ from json import load as l
 from json import dump as d
 
 def load(filename):
-    pass
+    return l(open(filename))
     
 def dump(object, filename):
-    pass
+    return d(object, open(filename, 'w'), ensure_ascii = 0, indent = 2)
+
+if __name__ == '__main__':
+    x = {1:2,2:3}
+    dump(x, 'x')
+    print(load('x'))
