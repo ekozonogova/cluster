d0ba783d89963acd32a500963cb9f6b6792105ae
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-09-18 11:15:12 +0500

    better unit test & messages

--- unilites.py
+++ unilites.py
@@ -1,13 +1,27 @@
 from json import load as l
 from json import dump as d
+from sys import stderr
 
 def load(filename):
-    return l(open(filename))
+    try:
+        print('Loading %s ...' % filename, end = '', file = stderr)
+        rez = l(open(filename))
+        print(' done', file = stderr)
+        return rez
+    except Exception as e:
+        print(' error! %s' % e, file = stderr)
+        raise e
     
 def dump(object, filename):
-    return d(object, open(filename, 'w'), ensure_ascii = 0, indent = 2)
+    print('Saving %s ...' % filename, end = '', file = stderr)
+    d(object, open(filename, 'w'), ensure_ascii = 0, indent = 2)
+    print('done', file = stderr)
 
 if __name__ == '__main__':
-    x = {1:2,2:3}
-    dump(x, 'x')
-    print(load('x'))
+    try:
+        print(load('x'))
+    except:
+        x = {1:2,2:3}
+        dump(x, 'x')
+        print(load('x'))
+        
