b02883e6e78bfd7f0cb6d6dbc1da2e29694b9db7
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-10-27 18:05:48 +0500

    clustering

--- stage4.py
+++ stage4.py
@@ -7,7 +7,22 @@ from sympy import diff, symbols
 from utilites import load 
 from constants import MIN_WEIGHT
 
+def filter_clusters(cluster_dict):
+    rez = {}
+    for key in cluster_dict:
+        x = [key]
+        x += cluster_dict[key]
+        x = tuple(sorted(x))
+        rez.update({x:1})
+    rex = []
+    for r in rez:
+        if len(r) > 1:
+            rex += [r]
+    return rex
+        
+
 def make_clusters(A,B,LV,index):
+    from pprint import PrettyPrinter
     CL = {}
     for i in range(len(LV)):
         y = []
@@ -19,27 +34,41 @@ def make_clusters(A,B,LV,index):
         y.reverse()
         n = 0
         for i, j, v in y:
-            if v < y[0][2] / 2:
+            if v < y[0][2] / 1.5:
                 break
             n += 1
-        if y[0] > 0.7:
-            CL.update({i:x[0:n]})
+        if y[0][2] > 0.7:
+            CL.update({i:y[0:n]})
 #            CL.update({i:x})
-    print(dumps(CL, indent=2, ensure_ascii = 1))
+
+    CLL = []
+    for i in CL:
+        CLL += [CL[i]]
+#    PrettyPrinter().pprint(CLL)
+    CLL = sorted(CLL, key = lambda x: x[0][2] )
+    CLL.reverse()
     rez = {}
     clustered = []
-    for i in CL.keys():
-        i_name = index[str(i)]
-        cnt = []
-        for j in range(len(CL[i])):
-            try:
-                j_name = index[str(CL[i][j][1])]
-            except IndexError:
-                print(CL, i, j)
-            if j_name not in clustered:
-                cnt += [j_name]
-                clustered += [j_name]
-        rez.update({i_name:cnt})
+    n = 0
+    for i in range(len(CLL)):
+        i_name = index[str(CLL[i][0][0])]
+        if i_name not in clustered:
+            clustered += [i_name]
+            cnt = []
+            for j in range(len(CLL[i])):
+                j_name = index[str(CLL[i][j][1])]
+                if j_name not in clustered:
+                    cnt += [j_name]
+                    clustered += [j_name]
+            rez.update({i_name:cnt})
+    cnt = []
+    for i in index:
+        if index[i] not in clustered:
+            cnt += [index[i]]
+        try:
+            rez.update({'empty':cnt})
+        except IndexError:
+            pass
             
     return rez
     
@@ -49,5 +78,6 @@ if __name__ == '__main__':
     B   = load('B.dat')
     LV  = load('LV.dat')
     idx = load('index.json')
-    print(make_clusters(A, B, LV, idx))
+    print(dumps(filter_clusters(make_clusters(A, B, LV, idx)), indent=2, ensure_ascii=0))
+#    print(dumps(make_clusters(A, B, LV, idx), indent=2, ensure_ascii=0))
     
