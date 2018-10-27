b3c97e6e0866f34a5b142f8d893acd43bff73f68
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-10-27 15:47:56 +0500

    diff

--- stage4.py
+++ stage4.py
@@ -1,4 +1,8 @@
-from numpy import savetxt
+from numpy import savetxt, poly1d
+from scipy import polyfit, linspace
+from json import dumps
+import matplotlib.pyplot as plt
+from sympy import diff, symbols
 
 from utilites import load 
 from constants import MIN_WEIGHT
@@ -6,22 +10,40 @@ from constants import MIN_WEIGHT
 def make_clusters(A,B,LV,index):
     CL = {}
     for i in range(len(LV)):
-        x = []
+        y = []
         for j in range(len(LV)):
             w = LV[i,j]
             if w > MIN_WEIGHT and i != j:
-                x += [(i,j,w)]
-        x = sorted(x, key = lambda x: x[2])
-        x.reverse()
-        if x[0][2] > 0.7:
-            CL.update({i:x[0:2]})
+                y += [(i,j,w)]
+        y = sorted(y, key = lambda x: x[2])
+        y.reverse()
+
+        x = linspace(0, len(y), len(y))
+        y = [ _[2] for _ in y ]
+        f = poly1d(polyfit(x,y,5))
+        x1 = symbols('x')
+        f1 = [ eval(str(diff(f(x1), x1, 1))) for x in x ]
+        f2 = [ eval(str(diff(f(x1), x1, 2))) for x in x ]
+        n = 0
+        print(f1, f2)
+#        for _ in [ eval(str(f1)) for x in x ]:
+#            if abs(_) < 0.0002:
+#                break
+#            n += 1
+        if y[0] > 0.7:
+            CL.update({i:x[0:n]})
+#            CL.update({i:x})
+    print(dumps(CL, indent=2, ensure_ascii = 1))
     rez = {}
     clustered = []
     for i in CL.keys():
         i_name = index[str(i)]
         cnt = []
         for j in range(len(CL[i])):
-            j_name = index[str(CL[i][j][1])]
+            try:
+                j_name = index[str(CL[i][j][1])]
+            except IndexError:
+                print(CL, i, j)
             if j_name not in clustered:
                 cnt += [j_name]
                 clustered += [j_name]
