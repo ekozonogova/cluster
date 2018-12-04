8515e76e04236de4a15e92d1e14d4d8d2edb9239
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-12-04 18:47:40 +0500

    soft compare + quiet mode

--- utilites.py
+++ utilites.py
@@ -22,14 +22,14 @@ def load(filename):
         print(' error! %s' % e, file = stderr)
         raise e
     
-def dump(object, filename):
+def dump(object, filename, quiet = 0):
     filetype = filename.split('.')[-1]
-    print('Saving %s ...' % filename, end = '', file = stderr)
+    if not quiet: print('Saving %s ...' % filename, end = '', file = stderr)
     if filetype == 'json':
         jdump(object, open(filename, 'w'), indent = 2, ensure_ascii = 0)
     elif filetype == 'dat':
         ndump(open(filename, 'wb'), object)
-    print('done', file = stderr)
+    if not quiet: print('done', file = stderr)
 
 """
 LM - матрица связей
@@ -133,6 +133,13 @@ def dict_to_xls(filename = 'out.xlsx',
     
     wb.save(filename)
 
+def compare(S1,S2):
+	ngrams = [S1[i:i+3] for i in range(len(S1))]
+	count = 0
+	for ngram in ngrams:
+		count += S2.count(ngram)
+
+	return count/max(len(S1), len(S2))
 
 if __name__ == '__main__':
     x = load("calculated_lq.json")
