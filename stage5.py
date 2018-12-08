9572c543d2bc8a6cfa3a4491ca87930646329ff5
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-12-08 21:36:47 +0500

    hi-lo regions

--- stage5.py
+++ stage5.py
@@ -5,9 +5,10 @@ from okpd2okved import okpd2okved, okved
 from rutermextract import TermExtractor as TE
 from requests import get, post
 from utilites import dump, load
-from regions import regions
+from regions import regions, get_yandex_name, emiss_to_yandex
 
 import numpy as np
+from pysal import W, Moran, Moran_Local
 
 FNAME = 'out.4.json'
 te = TE()
@@ -34,7 +35,7 @@ def download_fedstat():
 def filter_fedstat(FD = []):
     for item in FD:
         if item['dim32155'] in okved and \
-           item['dim32251'] in regions:
+           item['dim32251'] in [ regions[r]['emissname'] for r in regions.keys() ]:
             yield item
         
 def make_structure(FD = []):
@@ -42,7 +43,7 @@ def make_structure(FD = []):
     rez_ind = {}
     kyears = range(2009, 2017)
     for item in FD:
-        region = item['dim32251']
+        region = emiss_to_yandex(item['dim32251'])
         industry = item['dim32155']
         year_values = {}
         for ky in kyears:
@@ -208,37 +209,27 @@ def make_spec_data(IN = {}, CL = {}):
                 
     return {'По регионам':rez_reg, 'По кластерам':rez_clu }
 
-def calc_w_distance(IN = {}):
-    n = int(len(IN.keys()) / 2)
-    rez = np.zeros([n,n])
-    regions = sorted([ r.split('<->')[0] for r in IN.keys() ])
-    for i in range(n):
-        for j in range(n):
-            try:
-                rez[i,j] = 1/IN["%s<->%s" % (regions[i], regions[j])]
-            except KeyError:
-                pass
-    return rez
-        
-def calc_w_neighbors(IN = {}, DIST = {}):
-    regions = sorted(IN.keys())
-    n = len(IN.keys())
-    rez = {}
-    for i in range(n):
-        reg = IN[regions[i]]
-        try:
-            neighbors = reg['neighbors']
-            data = []
-            for neighbor in neighbors:
-                try:
-                    data += [1/DIST["%s<->%s" % (regions[i], neighbor)]]
-                except KeyError:
-                    data += [-1]
-            rez.update({i:data})
-        except KeyError as e:
-#            print(IN[regions[i]])
-            print(e)
-    return rez
+def get_hi_lo_regions(lq, regions, distances):
+    data = {}
+    A_dlq = 0
+    for cluster in ['Угольная промышленность', 'Высокотехнологичное оборудование и ИТ']:
+        for region in regions.keys():
+            lq_a = lq[region][cluster]['2016']
+            D_lq = 0
+            for neib in regions[region]['neighbors']:
+                lq_b = lq[neib][cluster]['2016']
+                D_lq += lq_a - lq_b
+            D_lq /= len(regions[region]['neighbors'])
+            A_dlq += D_lq
+            data.update({region:{cluster:D_lq})
+        A_dlq /= len(regions.keys())
+    res = {'hi':[],'lo':[]}
+    for region in data.keys():
+        if data[region] > A_dlq:
+            res['hi'].append({region:lq[region]['Высокотехнологичное оборудование и ИТ']['2016']})
+        else:
+            res['lo'].append({region:lq[region]['Высокотехнологичное оборудование и ИТ']['2016']})
+    return {'Высокотехнологичное оборудование и ИТ':res}
 
 if __name__ == '__main__':
     end = False
@@ -252,9 +243,8 @@ if __name__ == '__main__':
 
             calculated_sums = load('calculated_sums.json')
             calculated_lq = load('calculated_lq.json')
-            w_distance =  load('w_distance.dat')
-            w_neighbors =  load('w_neighbors.json')
-#            spatial_corr =  load('spatial_corr.json')
+            distances = load('distances.json')
+            cluster_hi_lo = load('cluster_hi_lo.json')
             end = True
         except FileNotFoundError as e:
             if e.filename == 'fedstat_data.json':
@@ -278,94 +268,8 @@ if __name__ == '__main__':
             if e.filename == 'calculated_lq.json':
                 calculated_lq = calc_lq(calculated_sums, spec_data)
                 dump(calculated_lq, 'calculated_lq.json')
-            if e.filename == 'w_distance.dat':
+            if e.filename == 'cluster_hi_lo.json':
+                regions = load('regions.json')
                 distances = load('distances.json')
-                w_distance = calc_w_distance(distances)
-                dump(w_distance, 'w_distance.dat')
-            if e.filename == 'w_neighbors.json':
-                neighbors = load('regions.json')
-                w_neighbors = calc_w_neighbors(neighbors, distances)
-                dump(w_neighbors, 'w_neighbors.json')
-#exit(0)
-    
-#    cookie = dict(
-#        JSESSIONID = 'D13E2B221B5AA3293EB22B55D2499D27',
-#        _ym_uid    = '1539604103112361576',
-#        _ym_d      = '1539604103', 
-#        _ym_isad   = '1',
-#        __utma     = '63246520.935552422.1539604108.1539604108.1539604108.1',
-#        __utmc     = '63246520',
-#        __utmz     = '63246520.1539604108.1.1.utmcsr=yandex|utmccn=(organic)|utmcmd=organic',
-#        __utmb     = '63246520.4.10.1539604108',
-#    )    
-#        
-#    content_type = "application/x-www-form-urlencoded; charset=UTF-8"
-
-def get_longest_key(item):
-    l = 0
-    for k in item.keys():
-        if len(k) > l:
-            l = len(k)
-            rez = k
-    return rez
-
-def normalize(string):
-    x1 = set([ _.normalized for _ in te(string) ])
-    if not x1:
-        x1 = set([string])
-    m = 0
-    rez = ''
-    for x in list(okpd2okved.values()) + list(okpd2okved.keys()):
-        x2 = set([ _.normalized for _ in te(x) ])
-        n = len(x1 & x2) / min(len(x1), len(x2))
-        if n > m:
-            m = n
-            rez = x
-    return rez
-            
-    
-#k_region = 'dim32251'
-#k_industry = 'dim32155'
-#
-#employment_data = load(open(FNAME))['results']
-#years = [ str(x) for x in range(2009, 2017) ]
-#
-#industry = {}
-#regions = set([ x[k_region] for x in employment_data ])
-#
-#
-#for ind in okved:# сделать по другому!!!
-#    industry_data = {}
-#    for reg in regions:
-#        region_data = {}
-#        for item in employment_data:
-#            rez = {}
-#            if item[k_industry] == ind and \
-#               item[k_region] == reg:
-#                for year in years:
-#                    for key in item.keys():
-#                        try:
-#                            key.index(year)
-#                            rez.update({year:float(item[key])})
-#                            break
-#                        except ValueError:
-#                            rez.update({year:0})
-#            region_data.update({reg: rez})
-#        industry_data.update({ind: region_data})
-#
-#print(industry)
-        
-# industry = {
-#      'стальное литье' : {
-#          'Рязанская область' : {
-#              2009:1017,
-#              2010:1116,
-#              2011:921,
-#              2012:1015,
-#              2013:1072,
-#              2014:1017,
-#              2015:975,
-#              2016:920,
-#          }
-#      }
-# }
+                cluster_hi_lo = get_hi_lo_regions(calculated_lq, regions, distances)
+                dump(cluster_hi_lo, 'cluster_hi_lo.json')
