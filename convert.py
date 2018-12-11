6c0d2f6fffb9fd3e01519a921b2713c5205a7932
Alexandr Larionov <alarionov93@yandex.ru>
2018-12-12 01:16:41 +0500

    changed keywords and update colors, created colors codes generator

--- convert.py
+++ convert.py
@@ -5,17 +5,23 @@ Created on Tue Nov 27 19:38:22 2018
 
 @author: dan
 """
-from json import dump, load
+from utilites import dump, load
 res = []
-x = load(open('specs.json', 'r'))
-n = 0
+x = load('specs.json')
 
 for key1 in x:
-    subres = []
-    for key2 in x[key1]:
-        subres += [{"name": key2, "values": x[key1][key2]}]
-    color = 'c%s' % n
-    res += [{"name": key1, "values": subres, "color": color}]
-    n += 1
+	subres = []
+	colors = {}
+	n = 0
+	for c in load('region_colors.json'):
+		if c["name"] == key1:
+			color = 'c%s' % n
+			break
+		n += 1
+
+	for key2 in x[key1]:
+		subres += [{"name": key2, "values": x[key1][key2]}]
+
+	res += [{"name": key1, "values": subres, "color": color}]
   
-dump(obj=res, fp=open('specs2.json', 'w'), indent=4, ensure_ascii=False)
+dump(object=res, filename='specs3.json')
