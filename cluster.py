48eaf20f296a4edb314bbe89ee2fe53fb32e8609
Daniel S Kurushin <daniel.kurushin@gmail.com>
2018-12-21 19:16:45 +0500

    data moved

--- cluster.py
+++ cluster.py
@@ -15,45 +15,45 @@ if __name__ == '__main__':
 	end = False
 	while not end:
 		try:
-			xlsx_file = load('xlsx_file.json')
-			dict_data, codes = load('xlsx_content.json')
-			nmpy_data = load('nmpy_data.dat')
-			idx = load('index.json')
-			LV = load('LV.dat')
-			CL = load('CL.json')
-			M = load('M.dat')
+			xlsx_file = load('data/xlsx_file.json')
+			dict_data, codes = load('data/xlsx_content.json')
+			nmpy_data = load('data/nmpy_data.dat')
+			idx = load('data/index.json')
+			LV = load('data/LV.dat')
+			CL = load('data/CL.json')
+			M = load('data/M.dat')
 			graph(M, LV, CL, idx, 'filename', subgraphs=0)
 
 			end = True
 		except FileNotFoundError as e:
-			if e.filename == 'xlsx_file.json':
+			if e.filename == 'data/xlsx_file.json':
 				xlsx_file = download_data(URL)
-				dump(xlsx_file, 'xlsx_file.json')
-			elif e.filename == 'xlsx_content.json':
+				dump(xlsx_file, 'data/xlsx_file.json')
+			elif e.filename == 'data/xlsx_content.json':
 				dict_data, codes = convert_data(xlsx_file)
-				dump((dict_data, codes), 'xlsx_content.json')
-			elif e.filename == 'nmpy_data.dat':
+				dump((dict_data, codes), 'data/xlsx_content.json')
+			elif e.filename == 'data/nmpy_data.dat':
 				nmpy_data, idx = form_matrix(dict_data, codes)
-				dump(nmpy_data, 'nmpy_data.dat')
-				dump(idx, 'index.json')
-			elif e.filename == 'LV.dat':
+				dump(nmpy_data, 'data/nmpy_data.dat')
+				dump(idx, 'data/index.json')
+			elif e.filename == 'data/LV.dat':
 				X, Y = get_XY(nmpy_data)
 				XX = get_r(X)
 				YY = get_r(Y)
 				XY = get_xy(X, Y)
 				YX = get_xy(Y, X)
 				LV = get_lv(XX, YY, YX, XY)
-				dump(LV,'LV.dat')
+				dump(LV,'data/LV.dat')
 			elif e.filename == 'CL.json':
 				nmpy_data = get_ZeroDiag(nmpy_data)
 				A = get_AftLinks(nmpy_data)
 				B = get_PreLinks(nmpy_data)
-				dump(A, 'A.dat')
-				dump(B, 'B.dat')
+				dump(A, 'data/A.dat')
+				dump(B, 'data/B.dat')
 				M = get_ImpLinks(A, B)
-				dump(M, 'M.dat')
+				dump(M, 'data/M.dat')
 				CL = filter_clusters(make_clusters(A, B, LV, idx))
-				dump(CL,'CL.json')
+				dump(CL,'data/CL.json')
 				end = True
 			
 	
