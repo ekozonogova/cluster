40f1e4c6ae54e366e58e53f6aa901e1b3e044f4c
Alexandr Larionov <alarionov93@yandex.ru>
2019-11-26 17:18:52 +0500

    fixed identical region paint in case of only one identical

--- clusters_web/clusters_core/static/js/benchmark.js
+++ clusters_web/clusters_core/static/js/benchmark.js
@@ -159,8 +159,8 @@ function BenchViewModel() {
             var chosen_length = self.identicalRegionsVal();
             colors["c0"] = {}
             colors["c0"][self.selectedRegion().code] = "#ff0000";
-            for (var i = 0; i <= chosen_length; i++) {
-                var c = parseInt(255 - (i+1) * 255/chosen_length).toString(16);
+            for (var i = 0; i < chosen_length; i++) {
+                var c = parseInt(255 - (i+1) * 200/chosen_length).toString(16);
                 if (c.length < 2) {
                     c = "0" + c;
                 }
