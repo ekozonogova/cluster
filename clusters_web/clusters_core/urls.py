d1ad08233c287bbf6cacff637bd1d7b9be091c5d
Alexandr Larionov <alarionov93@yandex.ru>
2022-01-26 15:58:24 +0500

    fixed error in urls

--- clusters_web/clusters_core/urls.py
+++ clusters_web/clusters_core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	path('product-space', views.macroregions, name='macroregions'),
 	path('regions_list', views.regions_list, name='regions'),
 	path('i_regions_list/<str:reg_name>', views.identical_regions_list, name='identical_regions'),
-	path('m_region_members/<str:reg_name>', views.macro_region_members, name='macro_region_members')
+	path('m_region_members/<str:reg_name>', views.macro_region_members, name='macro_region_members'),
 	path('svg_img/<str:path>', views.svg_img, name='svg_img')
 ]
